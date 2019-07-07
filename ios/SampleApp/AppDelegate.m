8ce26dec83c8d0b5f7f341b93348e93e74791163
Marian Beuther <marian.beuther@peakconcepts.de>
2019-07-07 14:44:28 +0200

    zw

--- ios/SampleApp/AppDelegate.m
+++ ios/SampleApp/AppDelegate.m
@@ -23,7 +23,7 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"SampleApp"
                                             initialProperties:nil];
-//  [RNSplashScreen show];
+  [RNSplashScreen show];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
--- src/components/home/styles.js
+++ src/components/home/styles.js
@@ -2,7 +2,9 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
   Root: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   Title: {
     margin: 20
